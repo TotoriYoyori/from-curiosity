5486955edb7462105e833bc7557eb199ab2de9e5
Stan Nguyen <stan.mng@gmail.com>
2024-01-28 19:06:35 +0100

    ui update pt.2

--- Assets/Scripts/MoleculeCheck.cs
+++ Assets/Scripts/MoleculeCheck.cs
@@ -25,7 +25,7 @@ public class MoleculeCheck : MonoBehaviour
         molecule = firstElement.text + firstSubscript.text + secondElement.text + secondSubscript.text;
         manager.setMolecule(molecule);
 
-        if (molecule == "CH4" || molecule == "C2H4" || molecule == "O2" || molecule == "H2O" || molecule == "SO3" || molecule == "C2H6" || molecule == "C3H8" || molecule == "ClO3" || molecule == "NO3" || molecule == "CO3" || molecule == "NaCl" || molecule == "HCl")
+        if (molecule == "CH4" || molecule == "C2H4" || molecule == "O2" || molecule == "H2O" || molecule == "SO3" || molecule == "C2H6" || molecule == "C3H8" || molecule == "SO3" || molecule == "ClO3" || molecule == "NO3" || molecule == "CO3" || molecule == "NaCl" || molecule == "HCl")
         {
             attack.SetActive(true);
         }
